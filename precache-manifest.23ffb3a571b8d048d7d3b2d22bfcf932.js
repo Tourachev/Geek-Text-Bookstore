696a478c4ad26cd1adb8bf02401ebfc8fe373f0a
Slav <itrshv@me.com>
2019-09-08 17:04:58 -0400

    Updates

---
+++ precache-manifest.23ffb3a571b8d048d7d3b2d22bfcf932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f4e128994375e71fbe265e8a4e345e",
+    "url": "/FIU-CEN-TEAM8/index.html"
+  },
+  {
+    "revision": "96d00bac2ab61e5d9fe8",
+    "url": "/FIU-CEN-TEAM8/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4c8ed0f4c0a911fe75f6",
+    "url": "/FIU-CEN-TEAM8/static/js/2.24091619.chunk.js"
+  },
+  {
+    "revision": "96d00bac2ab61e5d9fe8",
+    "url": "/FIU-CEN-TEAM8/static/js/main.980a0b9f.chunk.js"
+  },
+  {
+    "revision": "7ec790e2d8d75d5bedac",
+    "url": "/FIU-CEN-TEAM8/static/js/runtime~main.d1238491.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FIU-CEN-TEAM8/static/media/logo.5d5d9eef.svg"
+  }
+]);
