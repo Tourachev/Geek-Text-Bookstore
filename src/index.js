60c9f593133921d1ce37b555584ca6117f358894
Slav <itrshv@me.com>
2019-09-08 20:16:52 -0400

    Changed Landing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    return(
+        <div>
+            <h1>Howdy Partners</h1>
+            <p>This is the landing for CEN4010 Team 8</p>
+        </div>     
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
