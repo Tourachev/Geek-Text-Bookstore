61d97c86dc32067b4c42c81acebff3b63e0492d9
Viacheslav Tourachev <itrshv@me.com>
2019-09-08 23:47:21 -0400

    Added CSS styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
 const App = () => {
-    return(
-        <div>
-            <h1>Howdy Partners</h1>
-            <p>This is the landing for CEN4010 Team 8</p>
-        </div>     
+    return (
+        <div className='body'>
+            <div className='jumbotron'>
+                <h1>Howdy Partners</h1>
+                <p>This is the landing for CEN4010 Team 8</p>
+            </div>
+
+            <div>
+                <p></p>
+            </div>
+        </div>
     );
+};
+
+const Body = () => {
+    return {};
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
