4f50b223d2f81df84962a1997ba8bf971fad8056
Viacheslav Tourachev <itrshv@me.com>
2019-09-09 00:10:34 -0400

    Changed Styling Added Link

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ import './App.css';
 const App = () => {
     return (
         <div className='body'>
-            <div className='jumbotron'>
-                <h1>Howdy Partners</h1>
-                <p>This is the landing for CEN4010 Team 8</p>
+            <div className='jumbotron header'>
+                <h1 className='display-1'>Howdy Partners</h1>
+                <h1 className='display-4'>
+                    This is the landing for CEN4010 Team 8
+                </h1>
+                <hr className='my-4' />
+                <p>Learn more about all software engineering everything</p>
+                <a
+                    className='btn btn-primary btn-lg'
+                    href='https://stackoverflow.com/questions/434414/what-is-the-most-evil-code-you-have-ever-seen-in-a-production-enterprise-environ'
+                    role='button'
+                >
+                    TAKE ME THERE
+                </a>
             </div>
 
             <div>
