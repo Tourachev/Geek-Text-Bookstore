ff80f27a86f43cef0b66f22f9d486d68db7d6f28
Slav <itrshv@me.com>
2019-09-10 21:47:30 -0400

    Added FontAwesome support and a footer.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
 const App = () => {
     return (
         <div className='body'>
             <div className='jumbotron header'>
-                <h1 className='display-1'>Howdy Partners</h1>
-                <h1 className='display-4'>
-                    This is the landing for CEN4010 Team 8
-                </h1>
-                <hr className='my-4' />
-                <p>Learn more about all software engineering everything</p>
-                <a
-                    className='btn btn-primary btn-lg'
-                    href='https://stackoverflow.com/questions/434414/what-is-the-most-evil-code-you-have-ever-seen-in-a-production-enterprise-environ'
-                    role='button'
-                >
-                    TAKE ME THERE
-                </a>
+                <div className='container'>
+                    <h1 className='display-1'>Howdy Partners</h1>
+                    <h1 className='display-4'>
+                        This is the landing for CEN4010 Team 8
+                    </h1>
+                    <hr className='my-4' />
+                    <p>Learn more about all software engineering everything</p>
+                    <a
+                        className='btn btn-primary btn-lg'
+                        href='https://stackoverflow.com/questions/434414/what-is-the-most-evil-code-you-have-ever-seen-in-a-production-enterprise-environ'
+                        role='button'
+                    >
+                        TAKE ME THERE
+                    </a>
+                </div>
             </div>
 
-            <div>
-                <p></p>
+            <div className='container'>
+                <ul>
+                    <h2>Currently included:</h2>
+                    <li>Bootstrap</li>
+                    <li>FontAwesome</li>
+                    <li>jQuery</li>
+                </ul>
             </div>
         </div>
     );
@@ -32,4 +39,4 @@ const Body = () => {
     return {};
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
