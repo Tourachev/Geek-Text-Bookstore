ff7252a3fa12276c1a5726e2ee2fa1c31255cd33
Slav <itrshv@me.com>
2019-09-12 00:30:52 -0400

    Server added with express and mariaDB support installed. Proxy routing done on client side.

---
+++ client/build/precache-manifest.62613aa1aa4c5876f0f8968e368938ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af8af33a9dc60fd428b140555fe2073",
+    "url": "/FIU-CEN-TEAM8/index.html"
+  },
+  {
+    "revision": "122fc6193a0346927f4d",
+    "url": "/FIU-CEN-TEAM8/static/css/main.9e118ad6.chunk.css"
+  },
+  {
+    "revision": "4c8ed0f4c0a911fe75f6",
+    "url": "/FIU-CEN-TEAM8/static/js/2.24091619.chunk.js"
+  },
+  {
+    "revision": "122fc6193a0346927f4d",
+    "url": "/FIU-CEN-TEAM8/static/js/main.675b8638.chunk.js"
+  },
+  {
+    "revision": "7ec790e2d8d75d5bedac",
+    "url": "/FIU-CEN-TEAM8/static/js/runtime~main.d1238491.js"
+  }
+]);
