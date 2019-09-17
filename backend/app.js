67e4d9cc121bfce529ea54025d6878f96b150c61
nvela031 <nvela031@fiu.edu>
2019-09-16 23:26:39 -0400

    created a new router for the purchase book section.

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,9 @@ const mariadb = require("mariadb");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+//purchase router
+const purchaseRouter = require("./routes/purchase");
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -28,20 +31,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+//purchase router
+app.use("/purchase", purchaseRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
@@ -75,5 +81,5 @@ module.exports = app;
 //     });
 
 app.listen(PORT, () => {
-    console.log("Magic on port 3001");
+  console.log("Magic on port 3001");
 });
--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Button, Header, List } from 'semantic-ui-react';
-import './css/index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Button, Header, List } from "semantic-ui-react";
+import "./css/index.css";
 
-import NavBar from './components/NavBar';
-import JumboHeader from './components/JumboHeader';
-import BrowseField from './components/BrowseField';
+import NavBar from "./components/NavBar";
+import JumboHeader from "./components/JumboHeader";
+import BrowseField from "./components/BrowseField";
 
-import './css/index.css';
+import "./css/index.css";
 
 class App extends Component {
-    state = { users: [] };
+  state = { users: [] };
 
-    componentDidMount() {
-        fetch('/users')
-            .then(res => res.json())
-            .then(users => this.setState({ users }));
-    }
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
 
-    render() {
-        return (
-            <div>
-                <div className='body'>
-                    <NavBar />
-                    <div>
-                        <JumboHeader />
-                        <BrowseField />
-                    </div>
-                    <h1>Users</h1>
-                    {this.state.users.map(user => (
-                        <div key={user.id}>{user.username}</div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="body">
+          <NavBar />
+          <div>
+            <JumboHeader />
+            <BrowseField />
+          </div>
+          <h1>Users</h1>
+          {this.state.users.map(user => (
+            <div key={user.id}>{user.username}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
