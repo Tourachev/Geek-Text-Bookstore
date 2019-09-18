7e22b9ed8a38e4a0ccbc040f3d63ae4770cab076
Shanna <ssit002@fiu.edu>
2019-09-18 13:59:15 -0400

    Edit data for the books

---
+++ backend/routes/data.js
@@ -0,0 +1,68 @@
+const bookData = [
+    {
+        bookID: 1,
+        author: 'Chinua Achebe',
+        title: "Things Fall Apart",
+        genre: 'Nonfiction',
+        rating: '4',
+        price: '9.99',
+        topseller: true,
+        imgLink: "https://images-na.ssl-images-amazon.com/images/I/91yNF5xdR4L.jpg",
+    },
+    {
+        bookID: 2,
+        author: 'J.K Rowling',
+        title: 'Harry Potter & the Goblet of Fire',
+        genre: 'Fiction',
+        rating: '4',
+        price: '33.59',
+        imgLink: "https://images-na.ssl-images-amazon.com/images/I/71ykU-RQ0nL._SY606_.jpg",
+    },
+    {
+        bookID: 3,
+        author: 'J.K Rowling',
+        title: 'Harry Potter & the Chamber of Secrets',
+        genre: 'Fiction',
+        rating: '5',
+        price: '33.59',
+        imgLink: "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg",
+    },
+    {
+        bookID: 4,
+        author: 'J.K Rowling',
+        title: 'Harry Potter & the Sorcerers Stone',
+        genre: 'Fiction',
+        rating: '3',
+        price: '33.59',
+        imgLink: "https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg",
+    },
+    {
+        bookID: 5,
+        author: 'J.K Rowling',
+        title: 'Harry Potter & the Order of the Pheonix',
+        genre: 'Fiction',
+        rating: '4',
+        price: '33.59',
+        imgLink: 'https://images-na.ssl-images-amazon.com/images/I/71xcuT33RpL._SY606_.jpg'
+    },
+    {
+        bookID: 6,
+        author: 'Steven King',
+        title: 'The Stand',
+        genre: 'Horror',
+        rating: '5',
+        price: '10.99',
+        imgLink: 'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+    },
+    {
+        bookID: 7,
+        author: 'Steven King',
+        title: 'The Shining',
+        genre: 'Horror',
+        rating: '5',
+        price: '10.99',
+        imgLink: 'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+    },
+];
+
+module.exports = bookData;
