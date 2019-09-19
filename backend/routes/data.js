db5c98e1717aa31a8a1ed3e6577fbf434da7d851
Slav <itrshv@me.com>
2019-09-19 17:59:26 -0400

    Data update

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -1,114 +1,114 @@
 const bookData = [
     {
         bookID: 1,
-        author: 'Chinua Achebe',
-        title: 'Things Fall Apart',
-        genre: 'Nonfiction',
-        rating: '4',
-        price: '9.99',
+        author: "Chinua Achebe",
+        title: "Things Fall Apart",
+        genre: "Nonfiction",
+        rating: "4",
+        price: "9.99",
         topseller: true,
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/91yNF5xdR4L.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/91yNF5xdR4L.jpg"
     },
     {
         bookID: 2,
-        author: 'J.K Rowling',
-        title: 'Harry Potter & the Goblet of Fire',
-        genre: 'Fiction',
-        rating: '4',
-        price: '33.59',
+        author: "J.K Rowling",
+        title: "Harry Potter & the Goblet of Fire",
+        genre: "Fiction",
+        rating: "4",
+        price: "33.59",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/71ykU-RQ0nL._SY606_.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/71ykU-RQ0nL._SY606_.jpg"
     },
     {
         bookID: 3,
-        author: 'J.K Rowling',
-        title: 'Harry Potter & the Chamber of Secrets',
-        genre: 'Fiction',
-        rating: '5',
-        price: '33.59',
+        author: "J.K Rowling",
+        title: "Harry Potter & the Chamber of Secrets",
+        genre: "Fiction",
+        rating: "5",
+        price: "33.59",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg"
     },
     {
         bookID: 4,
-        author: 'J.K Rowling',
-        title: 'Harry Potter & the Sorcerers Stone',
-        genre: 'Fiction',
-        rating: '3',
-        price: '33.59',
+        author: "J.K Rowling",
+        title: "Harry Potter & the Sorcerers Stone",
+        genre: "Fiction",
+        rating: "3",
+        price: "33.59",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg"
     },
     {
         bookID: 5,
-        author: 'J.K Rowling',
-        title: 'Harry Potter & the Order of the Pheonix',
-        genre: 'Fiction',
-        rating: '4',
-        price: '33.59',
+        author: "J.K Rowling",
+        title: "Harry Potter & the Order of the Pheonix",
+        genre: "Fiction",
+        rating: "4",
+        price: "33.59",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/71xcuT33RpL._SY606_.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/71xcuT33RpL._SY606_.jpg"
     },
     {
         bookID: 6,
-        author: 'Steven King',
-        title: 'The Stand',
-        genre: 'Horror',
-        rating: '5',
-        price: '10.99',
+        author: "Steven King",
+        title: "The Stand",
+        genre: "Horror",
+        rating: "5",
+        price: "10.99",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg"
     },
     {
         bookID: 7,
-        author: 'Steven King',
-        title: 'The Shining',
-        genre: 'Horror',
-        rating: '5',
-        price: '10.99',
+        author: "Steven King",
+        title: "The Shining",
+        genre: "Horror",
+        rating: "5",
+        price: "10.99",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg"
     },
     {
         bookID: 8,
-        author: 'Steven King',
-        title: 'The Mist',
-        genre: 'Horror',
-        rating: '5',
-        price: '10.99',
+        author: "Steven King",
+        title: "The Mist",
+        genre: "Horror",
+        rating: "5",
+        price: "10.99",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg"
     },
     {
         bookID: 9,
-        author: 'Mike Tyson',
-        title: 'Gonna Fly',
-        genre: 'Sci-Fi',
-        rating: '5',
-        price: '10.99',
+        author: "Mike Tyson",
+        title: "Gonna Fly",
+        genre: "Sci-Fi",
+        rating: "5",
+        price: "10.99",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg"
     },
     {
         bookID: 10,
-        author: 'Hillary Clinton',
-        title: '10,000 Emails',
-        genre: 'Horror',
-        rating: '5',
-        price: '10.99',
+        author: "Hillary Clinton",
+        title: "10,000 Emails",
+        genre: "Horror",
+        rating: "5",
+        price: "10.99",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg"
     },
     {
         bookID: 11,
-        author: 'Fyodor Dostoevsky',
-        title: 'The Idiot',
-        genre: 'Fiction',
-        rating: '5',
-        price: '100 .99',
+        author: "Fyodor Dostoevsky",
+        title: "The Idiot",
+        genre: "Fiction",
+        rating: "5",
+        price: "100.99",
         imgLink:
-            'https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg'
+            "https://images-na.ssl-images-amazon.com/images/I/81c8No6mSPL.jpg"
     }
 ];
 
