cce61d891e1213ce7211124a9c3755f61ca65b12
Dauntedmist <steven.valle003@gmail.com>
2019-09-19 19:02:26 +0000

    Modified the GET request.

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -3,11 +3,31 @@ var router = express.Router();
 
 const bookData = require("./data");
 const realData = require("../app");
+const bookFilter = require("../book-filter");
+const mariadb = require("mariadb/callback");
 
+const pool = mariadb.createPool({
+    host: "virt-servers.mynetgear.com",
+    port: 30000,
+    user: "team8",
+    password: "WehaveControl",
+    database: "GeekTextDB",
+    connections: 10
+    //rowsAsArray: true
+});
+
+//Have to execute the function in the body of the GET request
 router.get("/", function(req, res, next) {
     console.log("hey");
-    res.json(bookData);
-    console.log(realData.bookSort.byTitle("Harry Potta"));
+    //res.json(bookData);
+    bookFilter.byTitle("Harry Potta", pool, function(err, result, fields){
+        if (err){
+            console.log('Error :' + err);
+        }
+        else{
+            res.json(result);
+        }
+    });
 });
 
 module.exports = router;
