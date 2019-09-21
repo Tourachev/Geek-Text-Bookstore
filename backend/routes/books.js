22b85dcf9c2e185589aed2f1280c989dc9ce5c77
dauntedmist <steven.valle003@gmail.com>
2019-09-21 10:57:05 -0400

    Changed the db connection pool into an export, located in an external module.

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,24 +1,15 @@
+const POOL = require('../custom_modules/db-pool');
+
 var express = require('express');
 var router = express.Router();
 
 const bookFilter = require('../custom_modules/book-filter');
-const mariadb = require('mariadb/callback');
-
-const pool = mariadb.createPool({
-    host: 'virt-servers.mynetgear.com',
-    port: 30000,
-    user: "team8",
-    password: "WehaveControl",
-    database: "GeekTextDB",
-    connectionLimit: 2
-    //rowsAsArray: true
-});
 
 //Have to execute the function in the body of the GET request
 router.get('/', function(req, res, next) {
     console.log(req);
     //res.json(bookData);
-    bookFilter.byTitle(false, pool, function(err, result, fields) {
+    bookFilter.byTitle(false, POOL, function(err, result, fields) {
         if (err) {
             console.log('Error :' + err);
         } else {
