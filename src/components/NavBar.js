511ce135d24d12e663c1a13dbce56410700ae98c
nvela031 <nvela031@fiu.edu>
2019-09-29 15:58:44 -0400

    fixing some bugs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,98 +1,65 @@
 import React from "react";
 import Header from "./Header";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         {/* <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-toggle='collapse'
-                    data-target='#navbarSupportedContent'
-                    aria-controls='navbarSupportedContent'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                >
-                    <span className='navbar-toggler-icon'></span>
-                </button> */}
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+        >
+            <span className='navbar-toggler-icon'></span>
+        </button> */}
 
-                <div
-                    className='collapse navbar-collapse'
-                    id='navbarSupportedContent'
-                >
-                    <div id='navbar-left'>
-                        <ul className='navbar-nav navbar-items'>
-                            <li
-                                className='nav-item active'
-                                id='home-logo'
-                                onClick={Header.handleCLick}
-                            >
-                                <Link to="/"><i className='fas fa-home fa-lg'></i></Link>
-                            </li>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div id="navbar-left">
+            <ul className="navbar-nav navbar-items">
+              <li
+                className="nav-item active"
+                id="home-logo"
+                onClick={Header.handleCLick}
+              >
+                <Link to="/">
+                  <i className="fas fa-home fa-lg"></i>
+                </Link>
+              </li>
 
-                            <li>
-                                <div className='ui vertical large button' tabIndex='0'>
-                                    <div className='visible content'>
-                                        <Link to="/Browse">Browse</Link>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className='ui vertical large button' tabIndex='0'>
-                                    <div className='visible content'>
-                                        Wishlist
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div id='navbar-right'>
-                        <ul className='navbar-nav navbar-items'>
-                            <li>
-                                <div className='ui vertical large button' tabIndex='0'>
-                                    <div className='visible content'>
-                                        Sign in
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className='ui vertical large button' tabIndex='0'>
-                                    <div className='visible content'>
-                                        <Link to="/cart"><i className='shop icon'></i></Link>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+              <li>
+                <div className="ui vertical large button" tabIndex="0">
+                  <div className="visible content">
+                    <Link to="/Browse">Browse</Link>
+                  </div>
                 </div>
-              </div>
+              </li>
+              <li>
+                <div className="ui vertical large button" tabIndex="0">
+                  <div className="visible content">Wishlist</div>
+                </div>
+              </li>
             </ul>
           </div>
           <div id="navbar-right">
             <ul className="navbar-nav navbar-items">
               <li>
-                <Link to="/signUp">
-                  <div
-                    className="ui vertical large animated button"
-                    tabIndex="0"
-                  >
-                    <div className="hidden content">It's Free!</div>
-                    <div className="visible content">Sign-up</div>
-                  </div>
-                </Link>
+                <div className="ui vertical large button" tabIndex="0">
+                  <div className="visible content">Sign in</div>
+                </div>
               </li>
               <li>
-                <Link to="/login">
-                  <div
-                    className="ui vertical large animated button"
-                    tabIndex="0"
-                  >
-                    <div className="hidden content">Welcome!</div>
-                    <div className="visible content">Sign in</div>
+                <div className="ui vertical large button" tabIndex="0">
+                  <div className="visible content">
+                    <Link to="/cart">
+                      <i className="shop icon"></i>
+                    </Link>
                   </div>
-                </Link>
+                </div>
               </li>
             </ul>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Home from './components/routes/HomePage';
-import Cart from './components/routes/CartPage';
-import Browse from './components/routes/BrowsePage';
-import Profile from './components/routes/ProfilePage';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/routes/HomePage";
+import Cart from "./components/routes/CartPage";
+import Browse from "./components/routes/BrowsePage";
+import Profile from "./components/routes/ProfilePage";
 
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
-import './css/index.css';
+import "./css/index.css";
 // import Login from "./components/Login";
-import ApplicationForm from './components/ApplicationForm';
-import ValidationLoginForm from './components/ValidatedLoginForm';
-import { Home } from './Home/Home';
+import ApplicationForm from "./components/ApplicationForm";
+import ValidationLoginForm from "./components/ValidatedLoginForm";
+// import { Home } from './Home/Home';
 
 const routes = (
-    <Router>
-        <div>
-            <Route exact path='/' component={Home} />
-            <Route path='/browse' component={Browse} />
-            <Route path='/cart' component={Cart} />
-        </div>
-    </Router>
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/browse" component={Browse} />
+      <Route path="/cart" component={Cart} />
+      <Route path="/login" component={ValidationLoginForm} />
+      <Route path="/signUp" component={ApplicationForm} />
+    </div>
+  </Router>
 );
 /*class App extends Component {
     constructor() {
@@ -41,4 +43,4 @@ const routes = (
     }
 }*/
 
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById("root"));
