c1f43f133bf7c30ba9fa3944ecb81d17b1c72bf6
Shanna <ssit002@fiu.edu>
2019-09-29 15:11:27 -0400

    Add filter functionality. Works with Multiple Filters, but resetting is still bugged

--- src/components/routes/BrowsePage.js
+++ src/components/routes/BrowsePage.js
@@ -1,19 +1,14 @@
 import React from "react";
 
 import BrowseSection from "../screens/BrowseSection";
-import SideBar from "../SideBar";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
-
 class Body extends React.Component {
     render() {
         return (
             <div>
                 <NavBar />
-                <div id='browse-container'>
-                    <SideBar />
-                    <BrowseSection />
-                </div>
+                <BrowseSection />
                 <Footer />
             </div>
         );
