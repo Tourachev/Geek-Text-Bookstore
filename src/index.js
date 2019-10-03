c8748ca6205a0c385cb31f974668f11b977f9a0d
Viacheslav Tourachev <itrshv@me.com>
2019-10-03 11:26:32 -0400

    Adjusted the merge conflict

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import Home from "./components/routes/HomePage";
-import Cart from "./components/routes/CartPage";
-import Browse from "./components/routes/BrowsePage";
-import Profile from "./components/routes/ProfilePage";
-
-import { Route, BrowserRouter as Router } from "react-router-dom";
-
-import "./css/index.css";
-// import Login from "./components/Login";
-import ApplicationForm from "./components/ApplicationForm";
-import ValidationLoginForm from "./components/ValidatedLoginForm";
-// import { Home } from './Home/Home';
-
-const routes = (
-  <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/browse" component={Browse} />
-      <Route path="/cart" component={Cart} />
-      <Route path="/login" component={ValidationLoginForm} />
-      <Route path="/signUp" component={ApplicationForm} />
-    </div>
-  </Router>
-);
-/*class App extends Component {
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import Navbar from './components/NavBar';
 import Home from './components/routes/HomePage';
 import Cart from './components/routes/CartPage';
 import Browse from './components/routes/BrowsePage';
@@ -38,20 +8,13 @@ import Profile from './components/routes/ProfilePage';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import './css/index.css';
-
-// const routes = (
-//     <Router>
-//         <div>
-//             <Route exact path='/' component={Home} />
-//             <Route path='/browse' component={Browse} />
-//             <Route path='/cart' component={Cart} />
-//         </div>
-//     </Router>
-// );
+// import Login from "./components/Login";
+import ApplicationForm from './components/ApplicationForm';
+import ValidationLoginForm from './components/ValidatedLoginForm';
+// import { Home } from './Home/Home';
 
 class App extends Component {
-
-constructor() {
+    constructor() {
         super();
     }
 
@@ -64,18 +27,13 @@ constructor() {
                         <Route exact path='/' component={Home} />
                         <Route path='/browse' component={Browse} />
                         <Route path='/cart' component={Cart} />
-                        <Route path='/profile' component={Profile} />
+                        <Route path='/login' component={ValidationLoginForm} />
+                        <Route path='/signUp' component={ApplicationForm} />
                     </div>
                 </Router>
             </div>
         );
     }
-
-}*/
-
-ReactDOM.render(routes, document.getElementById("root"));
-
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
