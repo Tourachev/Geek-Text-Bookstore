9b0c0762dc7156f8060593832df13e2bb34ea138
dauntedmist <steven.valle003@gmail.com>
2019-10-17 04:37:22 -0400

    database connects to geektext now, new db definition.

--- backend/custom_modules/db-pool.js
+++ backend/custom_modules/db-pool.js
@@ -5,7 +5,7 @@ const POOL = mariadb.createPool({
     port: 30000,
     user: "team8",
     password: "WehaveControl",
-    database: "GeekTextDB",
+    database: "geektext",
     connectionLimit: 2,
     dateStrings: 'date'
     //rowsAsArray: true
