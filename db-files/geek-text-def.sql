d7a47cfbbcd7de0ed63d4644adb597b4556ae1c1
dauntedmist <steven.valle003@gmail.com>
2019-10-18 00:30:51 -0400

    updated db definition.

--- db-files/geek-text-def.sql
+++ db-files/geek-text-def.sql
@@ -17,10 +17,16 @@ create table if not exists book
     primary key(bookid)
 );
 
+create table if not exists credentials
+(
+    userid varchar(40),
+    password varchar(256),
+    primary key(userid)
+);
+
 create table if not exists userinfo
 (
-    userid int,
-    username varchar(40) unique,
+    userid varchar(40) unique,
     email varchar(100) unique,
     fname varchar(40),
     lname varchar(40),
@@ -29,42 +35,43 @@ create table if not exists userinfo
     homeaddress varchar(140),
     nickname varchar(40),
     password varchar(256),
-    primary key(userid)
+    foreign key(userid) references credentials(userid)
+        on update cascade   
 );
 
 create table if not exists paymentinfo
 (
-    username varchar(40),
+    userid varchar(40),
     ccnum int not null,
     cvv int(3) not null,
     name varchar(100) not null,
     zip int(5) not null,
     expdate date not null,
-    foreign key(username) references userinfo(username)
+    foreign key(userid) references credentials(userid)
         on update cascade,
-    constraint cc_const unique(ccnum, username)
+    constraint cc_const unique(ccnum, userid)
 );
 
 create table if not exists addresses
 (
-    username varchar(40),
+    userid varchar(40),
     state varchar(2),
     city varchar(40),
     address varchar(140),
     zip int(5),
-    foreign key(username) references userinfo(username)
+    foreign key(userid) references credentials(userid)
         on update cascade
 );
 
 create table if not exists shoppingcart
 (
-    username varchar(40),
+    userid varchar(40),
     bookid int,
     quantity int,
     price double,
     total double as (price * quantity),
-    foreign key(username) references userinfo(username)
+    foreign key(userid) references credentials(userid)
         on update cascade,
     foreign key(bookid) references book(bookid),
-    constraint book_const unique(username, bookid)
+    constraint book_const unique(userid, bookid)
 );
