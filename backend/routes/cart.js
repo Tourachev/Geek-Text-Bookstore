556decef116afb4f8fc92c0c58c65f61cc5f869f
dauntedmist <steven.valle003@gmail.com>
2019-10-18 00:57:54 -0400

    using correct query now.

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,11 +1,13 @@
-const log = require('../custom_modules/user-transac');
+const queries = require('../custom_modules/user-transac');
 
 var express = require('express');
 var router = express.Router();
 
 //
 router.post('/', (req, res) => {
-    log.login(req.body, (err, result) => {
+    console.log(req.body);
+    queries.getCart(req.body, (err, result) => {
+        console.log('here we are');
         console.log(result);
         res.json({ result: result });
     });
