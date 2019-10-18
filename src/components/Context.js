444424ea2dd3f7b8034c2dc838017558d6561817
Viacheslav Tourachev <itrshv@me.com>
2019-10-17 21:44:06 -0400

    Deleted comments

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,26 +1,5 @@
-import React from "react";
+import React from 'react';
 
 const Context = React.createContext();
 
-// export const Consumer = Context.Consumer;
-
-// class Provider extends React.Component {
-//     state = {
-//         username: "Bubba"
-//         // isLoggedIn: false
-//         // setStatus: () => {}
-//     };
-
-//     setStatus = isLoggedIn => {
-//         this.setState({ isLoggedIn });
-//     };
-
-//     render() {
-//         return (
-//             <Context.Provider value={this.state}>
-//                 {this.props.children}
-//             </Context.Provider>
-//         );
-//     }
-// }
 export default Context;
