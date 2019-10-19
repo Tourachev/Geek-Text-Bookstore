47725af1f6113e72ff5e8151c4f16a9faad0cbc9
Shanna <ssit002@fiu.edu>
2019-10-18 22:12:55 -0400

    Fix commit bugs

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,4 +1,8 @@
 const queries = require('../custom_modules/user-transac');
+const CART_ADDED = 2; //New shipping address added
+const CART_DELETED = 1; //shipping address removed
+const NOT_UNIQUE = 1; //Duplicate address on insert
+const QUERY_ERR = -1; //connection error in query
 
 var express = require('express');
 var router = express.Router();
