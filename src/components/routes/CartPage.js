904f5014eefafbc07d858780989599fd83e0d42c
Viacheslav Tourachev <itrshv@me.com>
2019-10-21 23:55:38 -0400

    Fix error

--- src/components/routes/CartPage.js
+++ src/components/routes/CartPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../Footer';
 import PurchaseSection from '../screens/PurchaseSection';
-import Context from './components/Context';
+import Context from '../Context';
 
 class CartPage extends React.Component {
     constructor(props) {
