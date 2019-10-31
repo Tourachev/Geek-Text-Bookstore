7e71202373f2516e14ef37dfe35f2c4e6da7a4da
Steven Villa <svill069@fiu.edu>
2019-10-30 22:18:42 -0400

    removed unnecessary functions and html

--- src/components/screens/SavedForLater.js
+++ src/components/screens/SavedForLater.js
@@ -13,34 +13,21 @@ class SavedForLater extends React.Component {
         this.state = {
             cartBooks: [], //Actual Books in the cart
             cartItems: [], //HTML of the Table
-            quantity: 0,
             username: this.props.username, //should use context here
-            totalPrice: 0.0
         };
         // this.addItems = this.addItems.bind(this);
-        this.changeQuantity = this.changeQuantity.bind(this);
-        this.getCartItems = this.getCartItems.bind(this);
-        this.removeCartItems = this.removeCartItems.bind(this);
+        this.getWishItems = this.getWishItems.bind(this);
+        this.removeWishItems = this.removeWishItems.bind(this);
     }
 
-    getCartItems(books) {
+    getWishItems(books) {
         let total = 0.0;
 
         //Below all books get mapped onto the cart. Delete after
         let cart = books.map(item => {
-            total += item.price * item.quantity;
             return (
                 <tr key={item.bookId}>
                     <td>{item.title}</td>
-                    <td>
-                        x
-                        <input
-                            class='purchase-input'
-                            type='number'
-                            value={item.quantity}
-                            onChange={this.changeQuantity.bind(this, item)}
-                        />
-                    </td>
                     <td>${item.price.toFixed(2)}</td>
                     <td>
                         <button type='button' class='btn btn-outline-dark'>
@@ -63,43 +50,13 @@ class SavedForLater extends React.Component {
         this.setState({ cartBooks: books, cartItems: cart, totalPrice: total });
         this.forceUpdate();
     }
-
-    changeQuantity(item, event) {
-        let value = parseInt(event.target.value, 10);
-        let id = item.bookID;
-        if (event.target.value == "") {
-            value = 0;
-        }
-        fetch("/cart/edit", {
-            method: "POST",
-            body: JSON.stringify({
-                quantity: value,
-                price: item.price,
-                total: (item.price * value).toFixed(2),
-                title: item.title,
-                userid: this.state.username,
-                bookid: item.bookid
-            }),
-            headers: { "Content-Type": "application/json" }
-        })
-            .then(res => res.json())
-            .then(newInfo => {
-                console.log("ITEM EDITED");
-            })
-            .catch(err => {
-                console.log(err);
-            });
-
-        this.state.cartBooks.find(book => book.bookID === id).quantity = value;
-        this.getCartItems(this.state.cartBooks);
-    }
-
-    removeCartItems(item) {
+    
+    removeWishItems(item) {
         this.state.cartBooks.splice(
             this.state.cartBooks.findIndex(book => book.bookID === item.bookid),
             1
         );
-        this.getCartItems(this.state.cartBooks);
+        this.getWishItems(this.state.cartBooks);
         fetch("/cart/delete", {
             method: "POST",
             body: JSON.stringify({
@@ -140,17 +97,11 @@ class SavedForLater extends React.Component {
                             <tr>
                                 <th>Book Title</th>
                                 <th>Quantity</th>
-                                <th>Price</th>
                                 <th></th>
                             </tr>
                         </thead>
                         <tbody>{this.state.cartItems}</tbody>
                     </Table>
-                    <div className='price-row'>
-                        <h3>
-                            Total Price: ${this.state.totalPrice.toFixed(2)}
-                        </h3>
-                    </div>
                 </div>
             </div>
         );
