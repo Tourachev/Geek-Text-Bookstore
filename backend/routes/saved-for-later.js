d8265ae900128fe249395d709ed04a47c8471a54
Viacheslav Tourachev <itrshv@me.com>
2019-11-01 21:51:10 -0400

    Router upadted

--- backend/routes/saved-for-later.js
+++ backend/routes/saved-for-later.js
@@ -8,8 +8,16 @@ const QUERY_ERR = -1; //connection error in query
 var express = require('express');
 var router = express.Router();
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    queries.getLater(req.body, (err, result) => {
+        console.log(result);
+        res.json({ result: result });
+    });
+});
+
 router.post('/insert', (req, res) => {
-    queries.addToSaveForLater(req.body, (err, result) => {
+    queries.addToLater(req.body, (err, result) => {
         if (err) {
             console.log(err);
             res.send(QUERY_ERR); // -1
@@ -22,3 +30,20 @@ router.post('/insert', (req, res) => {
         }
     });
 });
+
+router.post('/delete', (req, res) => {
+    queries.delLater(req.body, err => {
+        if (err) {
+            console.log(
+                'Error in /backend/routes/credit-info : ' +
+                    'from addPaymentInfo\n' +
+                    err
+            );
+            res.json(null);
+        } else {
+            res.json({ decision: result }); //insert successful
+        }
+    });
+});
+
+module.exports = router;
