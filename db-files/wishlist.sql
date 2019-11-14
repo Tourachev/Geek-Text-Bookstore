4850ef76a8919d9d7c2643ccb01916c6b832b389
dauntedmist <steven.valle003@gmail.com>
2019-11-13 19:53:08 -0500

    Added wishlist name to table.

--- db-files/wishlist.sql
+++ db-files/wishlist.sql
@@ -5,6 +5,7 @@ create table if not exists wishlist
     bookid int,
     userid varchar(40),
     title varchar(400),
+    listname varchar(60),
     listnum int(1) check (listnum < 4 and listnum > 0),
     foreign key(userid) references credentials(userid)
         on update cascade,
