3e5f2effe207f03356d116a264a7a3cfe9076efa
dauntedmist <steven.valle003@gmail.com>
2019-11-13 19:48:12 -0500

    New wishlist table definition.

---
+++ db-files/wishlist.sql
@@ -0,0 +1,13 @@
+use geektext;
+
+create table if not exists wishlist
+(
+    bookid int,
+    userid varchar(40),
+    title varchar(400),
+    listnum int(1) check (listnum < 4 and listnum > 0),
+    foreign key(userid) references credentials(userid)
+        on update cascade,
+    foreign key(bookid) references book(bookid),
+    constraint list_const unqiue(bookid, userid, listnum)
+);
