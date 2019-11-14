ceab5b7a0517c6feb00668273b95be0bbd2cb436
dauntedmist <steven.valle003@gmail.com>
2019-11-13 20:27:01 -0500

    Removed listname from wishlist table and created new listnames table.

--- db-files/wishlist.sql
+++ db-files/wishlist.sql
@@ -5,10 +5,20 @@ create table if not exists wishlist
     bookid int,
     userid varchar(40),
     title varchar(400),
-    listname varchar(60),
     listnum int(1) check (listnum < 4 and listnum > 0),
     foreign key(userid) references credentials(userid)
         on update cascade,
     foreign key(bookid) references book(bookid),
     constraint list_const unique(bookid, userid, listnum)
+);
+
+create table if not exists listnames
+(
+    listnum int(1),
+    listname varchar(60),
+    userid varchar(40),
+    foreign key(listnum) references wishlist(listnum),
+    foreign key(userid) references credentials(userid)
+        on update cascade,
+    constraint name_const unique(userid, listnum)
 );
