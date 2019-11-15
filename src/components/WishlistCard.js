af0a87d089764757e9b109bbc17726568b3d85d7
dauntedmist <steven.valle003@gmail.com>
2019-11-15 04:47:10 -0500

    Modified wishlists, tables now show up.

--- src/components/WishlistCard.js
+++ src/components/WishlistCard.js
@@ -1,19 +1,69 @@
 import React from 'react';
+import { Table } from 'reactstrap';
+import { Button } from 'react-bootstrap';
+import { Icon } from 'semantic-ui-react'
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 class WishlistCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            wishlist: [],
+            listBody: [],
+            listname: '',
+            listnum: 1,
+            username: ''
+        }
+        this.getBody = this.getBody.bind(this);
+        this.toCart = this.toCart.bind(this);
+    }
+
+    toCart() {
+
+    }
+
+    getBody(books) {
+        let body = books.map(entry => {
+            return (
+                <tr key={entry.bookid}>
+                    <td>{entry.title}</td>
+                    <td>
+                        <button type='button' class='btn btn-outline-dark'>
+                            Move to cart
+                        </button>
+                        <Button
+                            onClick={this.toCart.bind(this)}
+                            style={{
+                                backgroundColor: 'rgba(0,0,0,0)',
+                                border: 'none'
+                            }}
+                        >
+                            <Icon name='close' color='red'/>
+                        </Button>
+                    </td>
+                </tr>
+            );
+        });
+        this.setState({ listBody: body });
     }
 
     render() {
-        //This don't work either yet.
-        //Not sure how to map the data being received
-        const list = this.props.wishlist.map(wishlist => this.whishlists);
-        return (
-            <div>
-                <h1>List: </h1>
-                {this.state.loading ? <CircularProgress /> : list}
+        var name = this.state.listname;
+        return(
+            <div id='purchase-container'>
+                <div id='purchase-body'>
+                    <h1 className='display-4' style={{ marginBottom: '3%' }}>
+                        { name }
+                    </h1>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Book Title</th>
+                            </tr>
+                        </thead>
+                        <tbody>{this.state.listBody}</tbody>
+                    </Table>
+                </div>
             </div>
         );
     }
