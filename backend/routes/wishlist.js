45c803e0702b117eb9972df3f5d2c1834ed81f84
dauntedmist <steven.valle003@gmail.com>
2019-11-15 04:49:43 -0500

    imported new module for wishlist queries.

--- backend/routes/wishlist.js
+++ backend/routes/wishlist.js
@@ -1,10 +1,10 @@
-const queries = require('../custom_modules/user-transac');
+const queries = require('../custom_modules/list-transac');
 var express = require('express');
 var router = express.Router();
 
 //body should be {username: ''}
 router.post('/', (req, res, next) => {
-    queries.getWishlistInfo(req.body.username, (err, result) => {
+    queries.getWishlists(req.body.username, (err, result) => {
         if (err) {
             console.log(
                 'Error in /backend/routes/credit-info : ' +
