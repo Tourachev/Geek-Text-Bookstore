81af253dc3cd88d294e502976cec81a9425bb955
dauntedmist <steven.valle003@gmail.com>
2019-11-16 00:42:58 -0500

    looking for changes.

--- backend/routes/wishlist.js
+++ backend/routes/wishlist.js
@@ -2,20 +2,20 @@ const queries = require('../custom_modules/list-transac');
 var express = require('express');
 var router = express.Router();
 
-//body should be {username: ''}
-router.post('/', (req, res, next) => {
-    queries.getWishlists(req.body.username, (err, result) => {
+//body should be {userid: '', listnum: int}
+router.post('/', (req, res) => {
+    console.log(req.body);
+    queries.getWishLists(req.body, (err, result) => {
         if (err) {
-            console.log(
-                'Error in /backend/routes/credit-info : ' +
-                    'from getWishlistInfo\n' +
-                    err
-            );
+            console.log('Error getting wishlist');
+            console.log(err);
             res.json(null);
         } else {
+            console.log(result);
             res.json(result);
         }
     });
 });
+
 
 module.exports = router;
