ad927cec42d3cb5a74d1c762499e752bd69b6ad3
dauntedmist <steven.valle003@gmail.com>
2019-11-21 14:58:18 -0500

    Somewhat finalized WishlistPage.

--- src/components/routes/WishlistPage.js
+++ src/components/routes/WishlistPage.js
@@ -1,45 +1,35 @@
 import React from 'react';
 import Footer from '../Footer';
 import { Link } from 'react-router-dom';
+import { Button, DropdownButton, Dropdown, Modal } from 'react-bootstrap';
+import { Icon } from 'semantic-ui-react';
+import { Table } from 'reactstrap';
 import Context from '../Context';
-import WishlistCard from '../WishlistCard';
+import WishlistPageWrapper from './WishlistPageWrapper';
 
 class Wishlist extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            username: this.props.username,
-            isLoggedIn: this.props.isLoggedIn,
-        };  
     }
 
-    //Component must be dynamic, so here we are.....
-    //Context is implemented. After user is logged in, the compenent will receive the data as props and execute queries.
-
     render() {
-        if (this.state.isLoggedIn) {
+        if (this.props.isLoggedIn) {
             return (
-                <div>
-                    <div className='body'>
-                    <WishlistCard 
-                        listnum={1}
-                        username={this.state.username}
-                    />
-                    <WishlistCard 
-                        listnum={2}
-                        username={this.state.username}
-                    />
-                    <WishlistCard 
-                        listnum={3}
-                        username={this.state.username}
-                    />
-                    <Footer />
+                <div><div>
+                    <h1 className='display-3'>
+                        Your Personal Wishlists 
+                    </h1>
+                    <div className="body">
+                        <WishlistPageWrapper
+                            username={this.props.username}
+                            isLoggedIn={this.props.isLoggedIn}
+                        />
+                    <Footer/>
+                    </div>
                     </div>
                 </div>
             );
-        }
-        else {
+        } else {
             return (
                 <div class='ohoh'>
                     <h1 class='display-4'>Oh, Oh!</h1>
@@ -60,6 +50,7 @@ class Wishlist extends React.Component {
             );
         }
     }
+
 }
 
 export default Wishlist;
