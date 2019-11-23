eb5b3289ca658868de8a4588b150826d4fc7b376
dauntedmist <steven.valle003@gmail.com>
2019-11-22 21:13:47 -0500

    Weird error.

--- src/components/routes/ViewBookPage.js
+++ src/components/routes/ViewBookPage.js
@@ -2,11 +2,7 @@ import React from 'react';
 import Footer from '../Footer';
 import DetailsSection from '../screens/BookDetailsSection';
 import {withRouter} from 'react-router-dom';
-<<<<<<< HEAD
 import Context from '../Context';
-=======
-import Context from './components/Context';
->>>>>>> a206b2c3d325ea172a0cb2ddcdb628684b334759
 
 class ViewBookPage extends React.Component {
 	constructor(props) {
@@ -20,13 +16,9 @@ class ViewBookPage extends React.Component {
 					{context => (
 						<div className='body'>
 							<DetailsSection
-<<<<<<< HEAD
                                 bookid={this.props.match.params.id}
                                 username={context.username}
                                 isLoggedIn={context.isLoggedIn}
-=======
-								bookid={this.props.match.params.id}
->>>>>>> a206b2c3d325ea172a0cb2ddcdb628684b334759
 							/>
 							<Footer />
 						</div>
